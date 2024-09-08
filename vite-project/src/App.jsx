c78c42b43124ecaf5bffd0067a3091befef347db
dhruv <dhruvjpatel5@gmail.com>
2024-09-08 11:26:22 +0530

    Initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat autem at impedit. Incidunt, totam nemo reprehenderit asperiores facilis qui accusamus, ad quibusdam voluptas quo, sequi maxime aut laudantium nulla veritatis!</h1>
+    </div>
+  );
+}
+
+export default App;
